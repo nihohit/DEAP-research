6b31da9b81775d0e916f036f743f01d59b1b6cbc
Shachar <nihohit@gmail.com>
2015-07-10 13:45:18 +0300

    Write detailed participant results

--- functions/participantsResults.m
+++ functions/participantsResults.m
@@ -1,7 +1,23 @@
 a = figure('visible','off');
-participantResult = zeros(40,201);
+index = 2;
+validValues = find(labels(:,index) > 6 | labels(:,index) < 4);
+numberOfValid = length(validValues);
+participantResult = zeros(numberOfValid+1,201);
+participantResult(1,1) = 'class';
 for i = 1:40
-    participantResult(i, :) = GetVideoVector(squeeze(data(i,:,:)),1);
+    participantResult(1,2+(i-1)*5) = 'ch'+i+'_Theta';
+    participantResult(1,3+(i-1)*5) = 'ch'+i+'_Alpha';
+    participantResult(1,4+(i-1)*5) = 'ch'+i+'_LowBeta';
+    participantResult(1,5+(i-1)*5) = 'ch'+i+'_HighBeta';
+    participantResult(1,6+(i-1)*5) = 'ch'+i+'_Gamma';
 end
-csvwrite('../participant1.csv', participantResult);
+
+for i = 1:numberOfValid
+    label = 1;
+    if(labels(validValues(i),index) < 4)
+        label = -1;
+    end
+    participantResult(i+1, :) = GetVideoVector(squeeze(data(validValues(i),:,:)),label);
+end
+csvwrite('participant1.csv', participantResult);
 close all;
