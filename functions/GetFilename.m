e894d0afe6683a0c8a9140265371f581d3c3dd8f
Shachar <nihohit@gmail.com>
2017-03-13 08:31:19 +0200

    Remove unused functions

--- functions/GetFilename.m
+++
@@ -1,7 +0,0 @@
-function fileName = GetFilename(labelIndex)
-
-currentExperiment = 'segmented_15Segments_removedDeviations';
-
-name = GetLabelName(labelIndex);
-fileName = strcat('../data preprocessing results/' , name ,'_', currentExperiment,'_', date ,'.arff');
-close all;
