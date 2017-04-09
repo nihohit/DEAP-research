e9b6cb936f6a6b65536fe1f0e830083399f09678
Shachar <nihohit@gmail.com>
2017-04-09 10:39:55 +0300

    remove baseline

--- functions/participantsResults.m
+++ functions/participantsResults.m
@@ -7,6 +7,9 @@ load(fileName); %this loads the 'data' array.
 a = figure('visible','off');
 
 data = data(:,1:numOfChannels,:); %remove excess channels
+sizeOfData = size(data);
+secondLength = sizeOfData(3) / 63;
+data = data(:,:,secondLength*3:secondLength*63); %remove baseline signal.
 
 participantResult = zeros(numOfMovies,numOfSegments * numOfBands * numOfChannels + numOfLabels); 
 
